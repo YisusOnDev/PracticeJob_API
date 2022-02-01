6cbb8b2e961fdf3c601f1f4641b8290651ab3c58
Jesús Téllez <jesustellez.contacto@gmail.com>
2022-02-01 14:12:03 +0100

    More production config needed

--- PracticeJob.API/Program.cs
+++ PracticeJob.API/Program.cs
@@ -15,6 +15,7 @@ namespace PracticeJob.API
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls("http://0.0.0.0:5000");
                 });
     }
 }
